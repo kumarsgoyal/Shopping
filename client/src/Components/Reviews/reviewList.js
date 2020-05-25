cb329ee12d5351c30766c767732ecc4654546a4a
Puneet <47234953+PM74367@users.noreply.github.com>
2020-05-25 22:08:32 +0530

    Update reviewList.js
    added review delete option

--- client/src/Components/Reviews/reviewList.js
+++ client/src/Components/Reviews/reviewList.js
@@ -3,13 +3,16 @@ import Loader from 'react-loader';
 import Review from './review';
 import history from '../../history';
 import Button from '@material-ui/core/Button';
+import StarRating from 'react-star-ratings';
 
 class ReviewList extends Component{
     constructor(props){
         super(props);
         this.state={
             id:"",
+            isReviewed:false,
             reviews:[],
+            Review:{},
             loaded:true
         }
     }
@@ -21,12 +24,39 @@ class ReviewList extends Component{
         fetch(url).then(res=>res.json())
         .then(res=>this.setState({reviews:res.reviews,loaded:true}))
         .catch(()=>console.log('problem'))
+        let url2='http://localhost:5000/customer/isReviewed/'
+        url2+=id;
+        fetch(url2,{
+            method: 'GET',
+            headers: {
+                Accept: 'application/json', 'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+
+        }).then(res=>res.json())
+        .then(res=>this.setState({isReviewed:res.isReviewed,Review:res.Review}));
     }
 
     addReview=()=>{
         history.push({pathname:'/Product/AddReview',state:{id:this.state.id}})
     }
     
+    deleteReview=()=>{
+        let id=this.state.Review._id;
+        let url='http://localhost:5000/reviews/deleteReview/'
+        url+=id;
+        fetch(url,{
+            method:'DELETE',
+            headers:{
+                Accept: 'application/json', 'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        }).then(res=>window.location.reload())
+    }
+
+    editReview=()=>{
+
+    }
     render(){
         if(this.state.loaded&&this.state.reviews.length===0){
             return<div>
@@ -38,12 +68,33 @@ class ReviewList extends Component{
             </div>
         }
         else{
+            let userrev=<Button variant="contained" color="secondary" size="medium" onClick={this.addReview}>
+                Add Review
+            </Button>
+            if(this.state.isReviewed){
+                console.log(this.state.Review)
+                userrev=<div>
+                    <hr />
+                    <div><h3>Your review</h3></div>
+                    <div>Name:{this.state.Review.customer_name}</div>
+                    <StarRating 
+                    starRatedColor="red"
+                    rating={this.state.Review.rating}
+                    starDimension="20px"
+                    starSpacing="10px"
+                    />
+                    <div>Message:{this.state.Review.message}</div>
+                    <div>
+                        <button onClick={this.deleteReview}>Delete Review</button>
+                        <button onClick={this.editReview}>Edit Review</button>
+                    </div>
+                </div>
+            }
+
             return <Loader loaded={this.state.loaded}>
                 {/* {console.log(this.state.reviews)} */}
                 {this.state.reviews.map((review,index)=><Review key={index} rev={review}/>)}
-                <Button variant="contained" color="secondary" size="medium" onClick={this.addReview}>
-                    Add Review
-                </Button>
+                {userrev}
             </Loader>
         }
     }
