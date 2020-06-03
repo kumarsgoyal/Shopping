e8cd8090a227846ad52b370a500e3e026d9b73b1
Sahil Kumar <kumar.sgoyal@gmail.com>
2020-06-03 23:25:50 +0530

    stock

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -16,8 +16,8 @@ const productSchema = new Schema({
         required: true
     },
     stock: {
-        type: Number
-        // required: true
+        type: Number,
+        required: true
     },
     unit_sold: {
         type: Number,
@@ -68,7 +68,7 @@ const productSchema = new Schema({
 const productModel = mongoose.model('product', productSchema);
 productModel.addProduct = (req, callback) => {
     let body = req.body;
-    let product = {name: body.name, category_id: body.category_id,price: body.price, main_photo: body.main_photo, description: body.description, features: body.features, options: body.options, estimated_days_to_deliver_by_seller: body.estimated_days_to_deliver_by_seller};
+    let product = {name: body.name, category_id: body.category_id, stock: body.stock, price: body.price, main_photo: body.main_photo, description: body.description, features: body.features, options: body.options, estimated_days_to_deliver_by_seller: body.estimated_days_to_deliver_by_seller};
     product.seller_id = req.session.user_id;
     // console.log('product', product);
     productModel.create(product, callback); 
@@ -76,7 +76,7 @@ productModel.addProduct = (req, callback) => {
 
 productModel.updateProduct = (req, callback) => {
     let body = req.body;
-    let product = {name: body.name, category_id: body.category_id,price: body.price, main_photo: body.main_photo, description: body.description, features: body.features, options: body.options, estimated_days_to_deliver_by_seller: body.estimated_days_to_deliver_by_seller};
+    let product = {name: body.name, category_id: body.category_id, stock: body.stock, price: body.price, main_photo: body.main_photo, description: body.description, features: body.features, options: body.options, estimated_days_to_deliver_by_seller: body.estimated_days_to_deliver_by_seller};
     product.seller_id = req.session.user_id;
     // console.log('product', product);
     productModel.findOneAndUpdate({_id: req.body.product_id}, product, callback); 
